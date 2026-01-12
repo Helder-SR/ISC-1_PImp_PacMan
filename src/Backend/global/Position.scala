5b91423592ce30ac1c6c9210d22d23f11a3b7a7f
Axel Schneider <axel.schneider2003@gmail.com>
2026-01-12 22:20:35 +0100

    Implement game map management and end game logic

--- src/Backend/global/Position.scala
+++ src/Backend/global/Position.scala
@@ -1,8 +1,8 @@
 package Backend.global
 
 class Position {
-  protected var x: Int = 0;
-  protected var y: Int = 0;
+  protected var x: Int = -1;
+  protected var y: Int = -1;
 
   protected def definePosition(x: Int, y: Int): Unit = {
     this.x = x;
